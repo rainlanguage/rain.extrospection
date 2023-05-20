bbae05b34baa39a8b0adb457e9fc8ecc0d07b8b1
thedavidmeister <thedavidmeister@gmail.com>
2023-05-21 01:29:45 +0400

    wip on scanner

--- src/Extrospection.sol
+++ src/Extrospection.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: CAL
 pragma solidity =0.8.18;
 
-import {ERC165CheckerUpgradeable as ERC165Checker} from
-    "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165CheckerUpgradeable.sol";
+import {ERC165Checker} from
+    "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 
 /// @title Extrospection
 /// @notice Exposes certain information available to evm opcodes as public
